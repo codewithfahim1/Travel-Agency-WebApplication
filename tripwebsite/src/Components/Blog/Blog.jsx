7b7d916d7e47811241247330890784b54da2d92a
codewithfahim1 <fahimprogrammer404@gmail.com>
2025-11-01 14:31:31 +0430

    made some changes on navbar and blog Components

--- tripwebsite/src/Components/Blog/Blog.jsx
+++ tripwebsite/src/Components/Blog/Blog.jsx
@@ -1,5 +1,44 @@
 import React from 'react'
 import './blog.scss'
+import { BsArrowRightShort } from "react-icons/bs";
+
+//importing images
+import img from '../../Assets/Blog/Hindoghosh Mountain.jfif'
+import img2 from '../../Assets/Blog/Buda.jfif'
+import img3 from '../../Assets/Blog/Kabul.jfif'
+import img4 from '../../Assets/Blog/Shah-e-dushamshera.jfif'
+
+// we use mapping here to avoid repetition of code
+
+
+const Posts = [
+    {
+    id: 1,
+    postImage: img,
+    title: 'Beautiful Mountains',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro, odio!'
+    },
+     {
+    id: 2,
+    postImage: img2,
+    title: 'Beautiful Mountains',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro, odio!'
+    },
+     {
+    id: 3,
+    postImage: img3,
+    title: 'Beautiful Mountains',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro, odio!'
+    },
+     {
+    id: 4,
+    postImage: img4,
+    title: 'Beautiful Mountains',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro, odio!'
+    },
+]
+
+
 
 const Blog = () => {
     return (
@@ -16,24 +55,29 @@ const Blog = () => {
             </div>
 
             <div className="mainContainer grid">
-                <div className="singlePost grid">
+                {
+                    Posts.map(({id, postImage, title, desc}) => {
+                       return(
+                    <div className="singlePost grid">
                     <div className="imgDiv">
-                    <img sec="" alt='Image Name' />
-                </div>
+                    <img src={postImage} alt={title} />
+                    </div>
 
                 <div className="postDetails">
                     <h3>
-                        Title
+                        {title}
                     </h3>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro, odio!</p>
+                    <p>{desc}</p>
                 </div>
 
                 <a href="#" className="flex">
-                    
+                    <BsArrowRightShort className='icon'/>
                     Read More!
                 </a>
-                
                 </div>
+                       ) 
+                    })
+                }
             </div>
         </div>
         </section>
--- tripwebsite/src/Components/Footer/Footer.jsx
+++ tripwebsite/src/Components/Footer/Footer.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
 import './footer.scss'
+import { MdCardTravel } from "react-icons/md";
+import { ImFacebook } from "react-icons/im";
+import { BsTwitterX } from "react-icons/bs";
+import { AiFillInstagram } from "react-icons/ai";
 
 const Footer = () => {
-    return (
-        <div>Footer</div>
-    )
+  return (
+    <section className="footer section">
+      <div className="footerContainer container grid">
+
+        {/* Logo and Social Icons */}
+        <div className="logoDiv">
+          <div className="footerLogo flex">
+            <MdCardTravel className="icon" />
+            <span className="logoText">AtemadTravel</span>
+          </div>
+
+          <div className="socials flex">
+            <ImFacebook className="icon" />
+            <BsTwitterX className="icon" />
+            <AiFillInstagram className="icon" />
+          </div>
+        </div>
+
+        {/* Navigation Links */}
+        <div className="footerLinks">
+          <span className="linkTitle">Information</span>
+          <ul className="linksList">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Explore</a></li>
+            <li><a href="#">Travel</a></li>
+            <li><a href="#">Blog</a></li>
+          </ul>
+        </div>
+
+        {/* Contact Info */}
+        <div className="footerContact">
+          <span className="linkTitle">Contact Us</span>
+          <span className="phone">+93 732354353</span>
+          <span className="email">fahimnikseyar@gmail.com</span>
+        </div>
+
+      </div>
+    </section>
+  )
 }
 
 export default Footer
